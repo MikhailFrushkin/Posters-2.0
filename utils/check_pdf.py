e90e8f5c631567099088ddaa83ff140768f2fc03
MikhailFrushkin <muxazila@mail.ru>
2024-03-25 20:33:16 +0700

    проверка

--- utils/check_pdf.py
+++ utils/check_pdf.py
@@ -15,8 +15,9 @@ def check_pdf(file_path):
 
 
 def check_pdfs(folder_path):
-    for file_name in os.listdir(folder_path):
+    for index, file_name in enumerate(os.listdir(folder_path), start=1):
         if file_name.endswith(".pdf"):
+            print('\r', index, end='', flush=True)
             file_path = os.path.join(folder_path, file_name)
             if not check_pdf(file_path):
                 logger.error(f"Файл {file_name} содержит ошибки, Удален!")
